cc6ecb257c0fc41f33ecb1d243b23d1a95f2bb9a
programthis <kylebachan@gmail.com>
2017-04-18 22:42:46 -0400

    Updated mobile navbar

--- imports/ui/navbar/navbar.js
+++ imports/ui/navbar/navbar.js
@@ -1,1 +1,13 @@
+import { Template } from 'meteor/templating';
+
 import "./navbar.html";
+
+Template.navbar.events({
+	"click .navbarMobileToggle": function(evt) {
+		evt.preventDefault();
+		$(".navbarMobileLinks").slideToggle("fast");
+	},
+	"click .navbarMobileLinks li": function() {
+		$(".navbarMobileLinks").slideToggle("fast");
+	}
+});
