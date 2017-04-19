513d9c08ad943f23c0c957218916bc1dd4cabd3d
programthis <kylebachan@gmail.com>
2017-04-19 01:35:26 -0400

    Working on rotation

--- imports/ui/layout/layout.js
+++ imports/ui/layout/layout.js
@@ -31,7 +31,19 @@ Template.layout.onRendered(function() {
 		}
 	    angle+=angleIncrease;
 	    $(".recordContainer .record").rotate(angle);
-	},17);
+	}, 17);
+
+	// var rotation = function (){
+	// 	$(".recordContainer .record").rotate({
+	// 		angle: 0,
+	// 		animateTo: 360,
+	// 		callback: rotation,
+	// 		easing: function (x,t,b,c,d){        // t: current time, b: begInnIng value, c: change In value, d: duration
+	// 			return c*(t/d)+b;
+	// 		}
+	// 	});
+	// }
+	// rotation();
 
 	$(window).resize(function() {
 		windowSize = $(window).width();
