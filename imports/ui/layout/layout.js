bf12e6202bf8883e835eb001d993df4e5c1d67fb
programthis <kylebachan@gmail.com>
2020-05-04 15:12:43 -0400

    Volume control UI update

--- imports/ui/layout/layout.js
+++ imports/ui/layout/layout.js
@@ -76,6 +76,25 @@ Template.layout.events({
 	"click .play, click .recordContainer": function(evt) {
 		evt.preventDefault();
 		playSong();
+
+		//static noise generator
+		var c = document.createElement("c");
+		var ctx = c.getContext("2d", {alpha: false});       // context without alpha channel.
+		ctx.fillStyle = 'rgb(255,255,255)';
+		ctx.width = window.innerWidth;
+		var idata = ctx.createImageData(c.width, c.height); // create image data
+		var buffer32 = new Uint32Array(idata.data.buffer);  // get 32-bit view
+
+		(function loop() {
+			noise(ctx);
+			requestAnimationFrame(loop)
+		})()
+
+		function noise(ctx) {
+			var len = buffer32.length - 1;
+			while(len--) buffer32[len] = Math.random() < 0.5 ? 0 : -1>>0;
+			ctx.putImageData(idata, 0, 0);
+		}
 	},
 	"click .next": function(evt) {
 		evt.preventDefault();
